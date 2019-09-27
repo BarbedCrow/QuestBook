c3f87120c7fe59ddea80bace758eed98d2f140f4
BarbedCrow <barbedcrow@gmail.com>
2019-09-27 18:56:10 +0300

    Updates

---
+++ precache-manifest.a70a17a1317f3f56f84317f1f87ca6c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8234cd37af4df12025cf24e3d8cc25",
+    "url": "/QuestBook/index.html"
+  },
+  {
+    "revision": "404092ec2afad1bc5181",
+    "url": "/QuestBook/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "fca7dd55bc74f0e6b133",
+    "url": "/QuestBook/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "404092ec2afad1bc5181",
+    "url": "/QuestBook/static/js/2.06194453.chunk.js"
+  },
+  {
+    "revision": "fca7dd55bc74f0e6b133",
+    "url": "/QuestBook/static/js/main.58c8f6c1.chunk.js"
+  },
+  {
+    "revision": "fbba8e82028f85d4da76",
+    "url": "/QuestBook/static/js/runtime-main.a443ecff.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/QuestBook/static/media/persik.4e1ec840.png"
+  }
+]);
