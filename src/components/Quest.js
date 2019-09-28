5dd918e23f101fdea2c632aa29b7ae69414e234a
Konstantin Toropin <Konstantin.Toropin@jetbrains.com>
2019-09-28 21:48:32 +0300

    [front] add globals handling

--- src/components/Quest.js
+++ src/components/Quest.js
@@ -6,29 +6,57 @@ class Quest extends React.Component {
     constructor(props) {
         super(props);
         let model = this.download();
+        // type, varName, justifyName)
+        let acc = "{";
+        model.globals.forEach( (value, index) => {
+            if (index !== 0) acc += ",";
+            let params = JSON.stringify({
+                type: value.type,
+                justifyName: value.justifyName,
+                value: value.value
+            });
+            acc += `"${value.varName}" : ${params}`
+        });
+        acc += "}";
+        console.log(acc);
+        console.log(JSON.parse(acc));
         this.state = {
             model: model,
+            globals: JSON.parse(acc),
             index: model.startNode
         };
-    }
+        const varName = "i";
+        // eval(`function changeVariable(prevState, newValue) {\n        let newGlobal = JSON.parse(JSON.stringify(prevState));\n        newGlobal.${varName}.value = newValue}` +
+        //     `this.setState( prevState => ({
+        //     globals: changeVariable(prevState, prevState.globals.${varName}.value + 1)
+        // }));`);
+                    console.log("before");
 
-    download() {
-        return JSON.parse("{\"globals\":[{\"type\":0,\"varName\":\"i\",\"justifyName\":\"Количество\"}],\"startNode\":0,\"finishNodes\":[5,6],\"nodes\":[{\"id\":0,\"text\":\"hello\",\"pic\":null,\"links\":[{\"transition\":\"i want 1\",\"to\":1},{\"transition\":\"i want 2\",\"to\":2}]},{\"id\":1,\"text\":\"1\",\"pic\":null,\"links\":[{\"transition\":\"i want to end\",\"to\":6}]},{\"id\":2,\"text\":\"2\",\"pic\":null,\"links\":[{\"transition\":\"i want inc\",\"to\":3}]},{\"id\":3,\"links\":[{\"transition\":null,\"to\":4}],\"code\":\"i++\",\"isBranching\":false},{\"id\":4,\"links\":[{\"transition\":\"i want 4 where end\",\"to\":5}],\"code\":\"i++\",\"isBranching\":false},{\"id\":5,\"text\":\"4\",\"pic\":null,\"links\":[]},{\"id\":6,\"text\":\"end\",\"pic\":null,\"links\":[]}]}") }
+}
+
+    download() { return JSON.parse("{\"globals\":[{\"type\":0,\"varName\":\"i\",\"justifyName\":\"Количество\",\"value\":0}],\"startNode\":0,\"finishNodes\":[5,6],\"nodes\":[{\"id\":0,\"text\":\"hello\",\"pic\":null,\"links\":[{\"transition\":\"i want 1\",\"to\":1},{\"transition\":\"i want 2\",\"to\":2}]},{\"id\":1,\"text\":\"1\",\"pic\":null,\"links\":[{\"transition\":\"i want to end\",\"to\":6}]},{\"id\":2,\"text\":\"2\",\"pic\":null,\"links\":[{\"transition\":\"i want inc\",\"to\":3}]},{\"id\":3,\"links\":[{\"transition\":null,\"to\":4}],\"code\":\"function changeVariable(prevState, newValue) {\\n        \\n                        let newGlobal = JSON.parse(JSON.stringify(prevState)).globals;\\n        \\n                        newGlobal.i.value = newValue;\\n                        return newGlobal;\\n                        }\\nthis.setState( prevState => ({\\n            globals: changeVariable(prevState, prevState.globals.i.value + 1)\\n        }));\",\"isBranching\":false},{\"id\":4,\"links\":[{\"transition\":\"i want 4 where end\",\"to\":5}],\"code\":\"function changeVariable(prevState, newValue) {\\n        \\n                        let newGlobal = JSON.parse(JSON.stringify(prevState)).globals;\\n        \\n                        newGlobal.i.value = newValue;\\n                        return newGlobal;\\n                        }\\nthis.setState( prevState => ({\\n            globals: changeVariable(prevState, prevState.globals.i.value + 1)\\n        }));\",\"isBranching\":false},{\"id\":5,\"text\":\"4\",\"pic\":null,\"links\":[]},{\"id\":6,\"text\":\"end\",\"pic\":null,\"links\":[]}]}"); }
 
     execUntilBaseNode(index) {
         const node = this.getNode(index);
         if (!this.isBaseNode(node)) {
+            const evalAns = eval(node.code);
             if (node.links.length === 1) {
                 return this.execUntilBaseNode(node.links[0].to)
             }
-            const evalAns = eval(node.code);
             if (node.isBranching) {
                 const newNodeIndex = node.links.filter(edge => edge.transition === evalAns)[0].to;
-                console.log("new node index is " + newNodeIndex);
                 // todo check newNodeIndex is int
                 return this.execUntilBaseNode(newNodeIndex)
             } else {
-                console.log("it is just hernya");
+                function changeVariable(prevState, newValue) {
+                    let newGlobal = JSON.parse(JSON.stringify(prevState));
+                    newGlobal.k = newValue
+                }
+
+                this.setState( prevState => ({
+                    globals: changeVariable(prevState, prevState.globals.k + 1)
+                }));
+
                 // todo check evalAns is int
                 return this.execUntilBaseNode(evalAns)
             }
@@ -38,7 +66,7 @@ class Quest extends React.Component {
     }
 
     isBaseNode(node) {
-        return (node.text != null && node.links != null)
+        return !(typeof node.text === "undefined")
     }
 
     getNode(index) {
