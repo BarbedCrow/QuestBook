9465080a28b7f2f459bd1a297934fb49817e1176
BarbedCrow <barbedcrow@gmail.com>
2019-09-29 11:06:21 +0300

    Updates

---
+++ precache-manifest.7437ff7964043aa71baf691602d7b645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58db963bb7a67702bdcb1e66a0021aeb",
+    "url": "/QuestBook/index.html"
+  },
+  {
+    "revision": "7392877d23f370ca9c45",
+    "url": "/QuestBook/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "7392877d23f370ca9c45",
+    "url": "/QuestBook/static/js/2.e79cec49.chunk.js"
+  },
+  {
+    "revision": "e5e60e0f7fe562f39df9",
+    "url": "/QuestBook/static/js/main.6ae79977.chunk.js"
+  },
+  {
+    "revision": "fbba8e82028f85d4da76",
+    "url": "/QuestBook/static/js/runtime-main.a443ecff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/QuestBook/precache-manifest.529e179e05288adc1b2ea72749a8d3f7.js"
+  "/QuestBook/precache-manifest.7437ff7964043aa71baf691602d7b645.js"
 );
 
 self.addEventListener('message', (event) => {
