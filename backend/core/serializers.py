13ff23742258808f43d5f2b42572162f1f8c8086
ImpyAngel <impy.bian@gmail.com>
2021-06-10 23:07:19 +0300

    Remove all django

--- backend/core/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import *
-
-
-class QuestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Quest
-        fields = ("id", "data", "is_published", "title", "description")
